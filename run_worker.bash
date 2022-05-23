254ca1b631ca780c438fbaf03ea975ce3c875ff2
gcvalderrama <gregory.valderrama@scotiabank.com.pe>
2022-05-22 21:27:58 -0500

    add

--- run_worker.bash
+++ run_worker.bash
@@ -1,2 +1,2 @@
 export PATH="$HOME/.local/bin:$PATH" # rasppberry
-locust -f performance/SessionUser.py --worker --master-host=192.168.1.15
+locust -f performance/MixSession.py --worker --master-host=192.168.1.15
